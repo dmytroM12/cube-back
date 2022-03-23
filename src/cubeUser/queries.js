5809ba15bd4db0515c9a668024346f482370625c
icewind13 <60686573+icewind13@users.noreply.github.com>
2022-03-23 18:19:05 +0200

    Update queries.js

--- src/cubeUser/queries.js
+++ src/cubeUser/queries.js
@@ -3,7 +3,7 @@ const getUsers= "SELECT * FROM cubeuser";
 const getUserByEmail= "SELECT * FROM cubeuser WHERE email = $1";
 const checkEmailExists="SELECT cu FROM cubeuser cu WHERE cu.email=$1"
 const addUser="INSERT INTO cubeuser(email,password) VALUES ($1,$2)";
-const deleteUser="DELETE FROM cubeuser WHERE id=$1"
+const deleteUser="DELETE FROM cubeuser WHERE email=$1"
 module.exports={
     getUsers,
     getUserByEmail,
